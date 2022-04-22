7542209458edd25af9ab92c1e65cd520df056e66
StefanoMRC <stefano.mrc92@gmail.com>
2022-04-22 15:18:09 +0200

    ricevere dati da api

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,52 @@
 <template>
   <div id="app">
-
+    <HeaderComp @funzRicerca='metodoRicerca'/>
   </div>
 </template>
 
 <script>
 import "bootstrap"
+import HeaderComp from './components/header/HeaderComp.vue'
+import axios from 'axios';
+
 
 export default {
   name: 'App',
   components: {
-
-  }
+    HeaderComp
+  },
+  data() {
+    return {
+      testoRicerca:'',
+      apiKey:'95d5ec590516c2769c39d001e98413d8',
+      film:[],
+      serie:[]
+    }
+  },
+  methods: {
+    metodoRicerca(testo){
+          this.testoRicerca=testo;
+          axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&language=it&page=1&include_adult=false&query=${this.testoRicerca}`)
+      .then((res)=>{
+          console.log(res.data.results)
+          this.film=res.data.results
+          console.log(this.film)
+          
+      }),
+      axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${this.apiKey}&language=it&page=1&include_adult=false&query=${this.testoRicerca}`)
+      .then((res)=>{
+          console.log(res.data.results)
+          this.serie=res.data.results
+          console.log(this.serie)
+          
+      })
+  }}
 }
 </script>
 
 <style lang="scss">
-@import "bootstrap/dist/css/bootstrap.min.css";
+  @import "bootstrap/dist/css/bootstrap.min.css";
+  .bg_custom{
+    background: #2e3a46;
+  }
 </style>
---
+++ src/components/header/HeaderComp.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="d-flex justify-content-between align-imtems-cente p-3 bg_custom">
+        <img src="../../assets/img/Netflix-logo-500x281.png" alt="">
+        <div class="input-group mb-3 w-25 d-flex align-items-center">
+            <input type="text" class="form-control" placeholder="Cerca il tuo film" aria-label="Recipient's username"
+                aria-describedby="button-addon2" v-model="ricerca">
+            <button class="btn btn-outline-secondary bg-danger text-white" type="submit" id="button-addon2"
+                @click.prevent="$emit('funzRicerca', ricerca)" 
+            >Avvia Ricerca</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComp',
+        data() {
+            return {
+                ricerca:''
+            }
+        },
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    img{
+        height: 80px;
+    }
+
+</style>
