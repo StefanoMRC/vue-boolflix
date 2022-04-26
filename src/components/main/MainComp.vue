305e77d2397b810e82d25c5385244014f6528fb3
StefanoMRC <stefano.mrc92@gmail.com>
2022-04-26 12:47:39 +0200

    fine

--- src/components/main/MainComp.vue
+++ src/components/main/MainComp.vue
@@ -10,8 +10,8 @@
 </template>
 
 <script>
-import FilmComp from './componenti film&serie'
-import SerieComp from './componenti film&serie'
+import FilmComp from './componenti film&serie/FilmComp.vue'
+import SerieComp from './componenti film&serie/SerieComp.vue'
 export default {
   name: 'MainComp',
   components:{
